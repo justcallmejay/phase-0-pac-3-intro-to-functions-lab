3521315c0e5a3db3aa0a584e6202ee969234dbb2
jay <jaypifiverr@gmail.com>
2022-05-30 20:22:22 -0400

    commit tree 562b9e49f17bed94de2a9993ec91dc9ae919ccbb

---
+++ index.js
@@ -0,0 +1,38 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+    console.log(string.toLowerCase())
+}
+
+
+function sayHiToHeadphonedRoommate(string) {
+ 
+  if (string == string.toLowerCase()) {
+    return(`I can't hear you!`);   
+  } 
+    else if (string == string.toUpperCase()) {
+    return("YES INDEED!");
+  }
+    else (string == "Let's have dinner together")
+    return("I would love to!");
+}
+console.log(sayHiToHeadphonedRoommate("HELLO!"))
+//DON'T FORGET THE () AFTER toLOwerCase and toUpperCase 
+//Separate returns using {}
+//
+
+/*function sayHiToHeadphonedRoommate(string) {
+  if (string === string.toUpperCase);
+    return("YES INDEED!")
+}
+console.log(string(string.toUpperCase))*/
